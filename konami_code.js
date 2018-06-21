cc2c0d69532c9df65cb69e2dd994b34968a0ee44
Jenny K <jkam17@gmail.com>
2018-06-21 22:21:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+let i=0
+function fn(e){
+  alert(e.which)
+  console.log(e.which)
+  const k=parseInt(e.which)
 }
+
+document.body.addEventListener('keydown',fn
+  )
+}
+
+init()
